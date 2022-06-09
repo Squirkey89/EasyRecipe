593de3f3b7abc85ad77cfc4375721d95af5c86ce
Squirkey89 <squirke89@gmail.com>
2022-06-09 20:27:17 +0000

    Added all templates completed base.html and index.html also added views and url to render pages

---
+++ recipes/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.RecipeList.as_view(), name='home'),
+    path('recipe', views.recipe, name='recipe'),
+    path('create_recipe', views.create_recipe, name='create_recipe'),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),  
+]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Recipe
 
-# Create your views here.
+
+
+class RecipeList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
+
+
+def recipe(request):
+    """
+    renders recipe page
+    """
+    return render(request, "recipe.html")
+
+def create_recipe(request):
+  
+    return render(request, 'create_recipe.html')
+
+
+def signup(request):
+    """
+    renders signup page
+    """
+    return render(request, "signup.html")
+
+
+def login(request):
+    """
+    renders login page
+    """
+    return render(request, "login.html")
