c15dc556a8bd10e189fa0a514dfe5374019d34b3
Squirkey89 <squirke89@gmail.com>
2022-06-16 13:00:08 +0000

    Added sign up and login templates

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,9 +1,15 @@
+from crispy_forms.helper import FormHelper
 from django.forms import ModelForm
 from django import forms
 from .models import Recipe
+
 
 class RecipeForm(ModelForm):
     class Meta:
         model = Recipe
         fields = ['title', 'description', 'prep_time', 'cook_time', 
         'serves', 'ingredients', 'instructions', 'featured_image', ]
+        def __init__(self, *args, **kwargs):
+            super(RecipeForm, self).__init__(*args, **kwargs)
+            self.helper = FormHelper()
+            self.helper.form_show_labels = False 
